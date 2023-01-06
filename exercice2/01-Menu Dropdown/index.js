049cedd137d7e95eee91754a9d4c991311314f9e
Bluebender <olliviev@gmail.com>
2023-01-06 14:27:03 +0000

    Sauvegarde de 15h

--- exercice2/01-Menu Dropdown/index.js
+++ exercice2/01-Menu Dropdown/index.js
@@ -3,5 +3,13 @@
 
 window.addEventListener("DOMContentLoaded", function(){
 
-
+    let button = document.getElementById("button");
+    let liste = document.getElementById("hidden_liste");
+    let ext = document.querySelector("body");
+    
+    button.addEventListener("click", function(event){
+      liste.classList.toggle("hidden")
+    });
+    
+    
 });
